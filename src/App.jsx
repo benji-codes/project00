565076be6fb776f1054f70e4c4430e01bfe885c0
benji-codes <benjaminemmanuelchima@gmail.com>
2025-08-31 20:40:20 +0100

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import LoginStatus from './component/LoginStatus'
+import TaskItem from './component/Taskitem';
 import TaskList from './component/TaskList';
 import TaskManager from './component/TaskManager';
 
 function App() {
-  
+  const sampleTask = { id: 1, text: "learn react", completed: false }; 
+
   return (
-  <TaskManager />
+    <TaskItem task={sampleTask}/>
   )
 }
 
